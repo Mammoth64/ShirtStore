d2d9401e04befc2bc6915027b691b583cc836865
Mammoth64 <mammoth1907@gmail.com>
2022-06-12 13:48:29 +0200

    Divide the project into smaller components

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,7 @@ import productsData from '../../data/products';
 import Product from '../Product/Product';
 
 const Products = () => {
-  const [products]  = useState(productsData);
+  const [products] = useState(productsData);
 
   return (
     <section>
